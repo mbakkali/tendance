ad03890f63293f6cf18436bdf70475f66ff3c1ad
mbakkali <mbakkali@50bf7a57-d5b9-42b1-bcda-f83ccc8449be>
2016-06-09 13:48:17 +0000

    TEst svn

--- database/TendanceServer/src/main/java/server/Main.java
+++ database/TendanceServer/src/main/java/server/Main.java
@@ -1,38 +1,116 @@
+
 package server;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import server.dao.UserDAO;
-
+import java.sql.DriverManager;
 import java.sql.Connection;
-import java.util.List;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.ArrayList;
 
-import static server.SQLDatabase.ConnectDatabase;
-/*
-* Cette classe va disparaitre quand on aura plus besoin de fiare des tests
- */
+import java.text.*;
+
+
+
 
 public class Main {
 
-	public static void main(String[] args) {
-		ApplicationContext context =
-                new ClassPathXmlApplicationContext("Spring-Module.xml");
-		UserDAO userDAO = (UserDAO) context.getBean("userDAO");
+    public static void main(String[] args) {
 
-        User utilisateur1 = new User("utilisateur3","utilisateurpatrik@insamail.com","/img/utilisateur1","bio1",true,true,"0637263716","1994-02-14","utilisateur1password");
- 		System.out.println("utilisateur1 crée");
-        userDAO.insert(utilisateur1);
+        try {
+
+            Connection connection = ConnectDatabase();
+
+            DisplayTable(connection, "users");
+            //Test SVN
 
 
-        User user = userDAO.findByUserId(1);
-        System.out.println(user);
-        List<User> users = userDAO.findAll();
-        System.out.println(users);
-    	//DisplayTable(connection,"users")
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-//Connexion à la base de donnée du serveur 
 
 
 
-}
+
+    //Affiche la table
+    public static void DisplayTable(Connection connection, String table) {
+        try {
+
+            Statement state = connection.createStatement();
+            ResultSet result = state.executeQuery("SELECT * FROM "+table);
+            ResultSetMetaData resultMeta = result.getMetaData();
+            System.out.println("\n**********************************");
+
+            //On affiche le nom des colonnes
+            for(int i = 1; i <= resultMeta.getColumnCount(); i++)
+                System.out.print("\t" + resultMeta.getColumnName(i).toUpperCase() + "\t *");
+            System.out.println("\n**********************************");
+
+
+            while(result.next()){
+                for(int i = 1; i <= resultMeta.getColumnCount(); i++)
+                    System.out.print("\t" + result.getObject(i).toString() + "\t |");
+                System.out.println("\n---------------------------------");
+            }
+
+            result.close();
+            state.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static Connection ConnectDatabase() throws SQLException{
+
+        System.out.println("> Début de la connection à la base de données");
+
+        String url = "jdbc:mysql://90.66.114.198:3306/Tendance";
+        String user = "monitor";
+        String passwd = "tendance2016";
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+
+        } catch (ClassNotFoundException e) {
+
+            System.out.println("> Problème de JDBC Driver?");
+            e.printStackTrace();
+
+        }
+
+        System.out.println("> Le driver JDBC est bon!");
+
+        Connection connection = null;
+
+        try {
+
+            connection = DriverManager.getConnection(url,user,passwd);
+
+        } catch (SQLException e) {
+
+            System.out.println("> Problème de connexion à la base de données, différents sources possibles (pas les droits, dbb inexistante, ...)");
+            e.printStackTrace();
+
+        }
+
+        if (connection != null) {
+            System.out.println("> Vous avez le controle de la base de données Tendance");
+        } else {
+            System.out.println("> Problème de connexion à la base de données, différents sources possibles (pas les droits, dbb inexistante, ...)");
+        }
+        return connection;
+    }
+
+
+    }
+
